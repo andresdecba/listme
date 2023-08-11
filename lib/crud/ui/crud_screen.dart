68be9dae26288c46206fea3dbdec1b48f3402ca2
Andrés Pugliese <andresdecba@gmail.com>
2023-08-10 22:03:04 -0300

    avancess

--- lib/crud/ui/crud_screen.dart
+++ lib/crud/ui/crud_screen.dart
@@ -1,13 +1,13 @@
+import 'dart:ui';
+import 'package:animated_list_plus/animated_list_plus.dart';
+import 'package:animated_list_plus/transitions.dart';
 import 'package:flutter/material.dart';
-import 'package:great_list_view/great_list_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:listme/core/constants/constants.dart';
-import 'package:listme/core/constants/typedefs.dart';
 import 'package:listme/crud/models/item.dart';
 import 'package:listme/crud/models/lista.dart';
 import 'package:listme/crud/ui/widgets/create_task_bottomsheet.dart';
 import 'package:listme/crud/ui/widgets/input_item.dart';
-import 'package:listme/crud/ui/widgets/item_tile.dart';
 import 'package:listme/crud/ui/widgets/list_title.dart';
 import 'package:uuid/uuid.dart';
 
@@ -25,10 +25,6 @@ class _CrudScreenState extends State<CrudScreen> {
   late Box<Lista> _box;
   late Lista _dbList;
   late Uuid _uuid;
-  late GlobalKey<AnimatedListState> _globalKey;
-
-  final scrollController = ScrollController();
-  final controller = AnimatedListController();
 
   @override
   void initState() {
@@ -36,153 +32,125 @@ class _CrudScreenState extends State<CrudScreen> {
     _box = Hive.box<Lista>(AppConstants.listsCollection);
     _dbList = _box.get(widget.id)!;
     _uuid = const Uuid();
-    _globalKey = GlobalKey<AnimatedListState>();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // APPBAR //
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-        ),
+      // APP BAR //
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
 
-        // ADD BUTTON //
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            createTaskBottomSheet(
-              context: context,
-              showClose: true,
-              child: InputItem(
-                returnText: (value) {
+      // BUTTON //
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          createTaskBottomSheet(
+            context: context,
+            showClose: true,
+            child: InputItem(
+              returnText: (value) {
+                setState(() {
                   //guardar en la db
                   var id = _uuid.v4();
                   _dbList.items.add(Item(content: value, isDone: false, id: id));
                   _dbList.save();
-                  // agregar a la lista animada
-                  _globalKey.currentState!.insertItem(0, duration: const Duration(milliseconds: 400));
-                },
-              ),
-              enableDrag: true,
-            );
-          },
-          child: const Icon(Icons.add),
-        ),
+                });
+              },
+            ),
+            enableDrag: true,
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
 
-        // BODY //
-        body: Visibility(
-          visible: _dbList.items.isNotEmpty,
-          replacement: const Center(child: Text("No items")),
-          child: Column(
-            children: [
-              // TITLE //
-              ListTitle(
-                initialValue: _dbList.title,
-                returnText: (value) {
-                  setState(() {
-                    _dbList.title = value;
-                    _dbList.save();
-                  });
-                },
-              ),
+      // BODY //
+      body: Column(
+        children: [
+          // TÍTULO //
+          ListTitle(
+            initialValue: _dbList.title,
+            returnText: (value) {
+              _dbList.title = value;
+              _dbList.save();
+            },
+          ),
 
-              // ITEMS LIST //
-              MyCustomAnimatedList(
-                list: _dbList.items,
-                globalKey: _globalKey,
-                child: (i) {
-                  var item = _dbList.items[i];
-                  return ItemTile(
-                    text: item.content,
-                    isDone: item.isDone,
-                    onTapIsDone: () => _onDone(item),
-                    onTapClose: () => _onClose(item, i),
+          // COMIENZO LISTA //
+          ImplicitlyAnimatedReorderableList<Item>(
+            items: _dbList.items.reversed.toList(),
+            reverse: false,
+            shrinkWrap: true,
+            areItemsTheSame: (oldItem, newItem) => oldItem.id == newItem.id,
+            // Remember to update the underlying data when the list has been reordered.
+            onReorderFinished: (item, from, to, newItems) {
+              setState(() {
+                _dbList.items
+                  ..clear()
+                  ..addAll(newItems.reversed.toList());
+                _dbList.save();
+              });
+            },
+            itemBuilder: (context, itemAnimation, item, index) {
+              // Each item must be wrapped in a Reorderable widget.
+              return Reorderable(
+                // Each item must have an unique key.
+                key: ValueKey(item),
+                // The animation of the Reorderable builder can be used to
+                // change to appearance of the item between dragged and normal
+                // state. For example to add elevation when the item is being dragged.
+                // This is not to be confused with the animation of the itemBuilder.
+                // Implicit animations (like AnimatedContainer) are sadly not yet supported.
+                builder: (context, dragAnimation, inDrag) {
+                  final t = dragAnimation.value;
+                  final elevation = lerpDouble(0, 8, t);
+                  final color = Color.lerp(Colors.white, Colors.white.withOpacity(0.8), t);
+
+                  return SizeFadeTransition(
+                    sizeFraction: 0.7,
+                    curve: Curves.easeInOut,
+                    animation: itemAnimation,
+                    child: Material(
+                      color: color,
+                      elevation: elevation!,
+                      type: MaterialType.transparency,
+                      child: ListTile(
+                        title: Text(item.content),
+                        // The child of a Handle can initialize a drag/reorder.
+                        // This could for example be an Icon or the whole item itself. You can
+                        // use the delay parameter to specify the duration for how long a pointer
+                        // must press the child, until it can be dragged.
+                        trailing: const Handle(
+                          delay: Duration(milliseconds: 100),
+                          child: Icon(
+                            Icons.list,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
                   );
                 },
-              ),
-            ],
+              );
+            },
           ),
-        )
+        ],
+      ),
 
-        // ValueListenableBuilder(
-        //   valueListenable: _box.listenable(),
-        //   builder: (context, Box<Lista> box, _) {},
-        // ),
-        );
-  }
-
-  void _onDone(Item item) {
-    setState(() {
-      item.isDone = !item.isDone;
-      _dbList.save();
-    });
-  }
-
-  void _onClose(Item item, int i) {
-    setState(() {
-      _globalKey.currentState!.removeItem(i, (context, animation) {
-        return FadeTransition(
-          key: UniqueKey(),
-          opacity: animation,
-          child: SizeTransition(
-            key: UniqueKey(),
-            sizeFactor: animation,
-            child: ItemTile(
-              onTapIsDone: () {},
-              onTapClose: () {},
-              text: item.content,
-              isDone: item.isDone,
-            ),
-          ),
-        );
-      });
-      _dbList.items.remove(item);
-      _dbList.save();
-    });
-  }
-}
-
-class MyCustomAnimatedList extends StatefulWidget {
-  const MyCustomAnimatedList({
-    required this.list,
-    required this.child,
-    required this.globalKey,
-    super.key,
-  });
-
-  final List<dynamic> list;
-  final ReturnWidget child;
-  final GlobalKey<AnimatedListState> globalKey;
-
-  @override
-  State<MyCustomAnimatedList> createState() => _MyCustomAnimatedListState();
-}
-
-class _MyCustomAnimatedListState extends State<MyCustomAnimatedList> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedList(
-      key: widget.globalKey,
-      reverse: true,
-      physics: const BouncingScrollPhysics(),
-      shrinkWrap: true,
-      initialItemCount: widget.list.length,
-      itemBuilder: (context, index, animation) {
-        return FadeTransition(
-          key: UniqueKey(),
-          opacity: animation,
-          child: SizeTransition(
-            key: UniqueKey(),
-            sizeFactor: animation,
-            child: widget.child(index),
-          ),
-        );
-      },
+      /*
+      body: ValueListenableBuilder(
+        valueListenable: _box.listenable(),
+        builder: (context, Box<Lista> box, _) {
+          // SI ESTÁ VACÍA //
+          if (_dbList.items.isEmpty) {
+            return const Center(
+              child: Text("NO ITEMS"),
+            );
+          }
+          return ;
+        },
+      ),
+      */
     );
   }
 }
